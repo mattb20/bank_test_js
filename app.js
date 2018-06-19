248af5a50bce88c08bfebd31fd72e1cfea21fd60
mattb20 <matt_burgoyne@live.co.uk>
2018-06-19 09:57:44 +0100

    fixed require statement in which unexisting bank property was being assigned to repl context

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const repl = require('repl'),
   context = repl.start('$ ').context;
 
-  context.Bank = require('./Bank').Bank;
-  context.Transaction = require('./Transaction').Transaction;
+  context.Bank = require('./bank');
