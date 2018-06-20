947f1fd2f06bba91131a23c467ce2d90dcae3f17
mattb20 <matt_burgoyne@live.co.uk>
2018-06-20 16:04:26 +0100

    included an arrow function in bankspec, making a deposit

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -1,7 +1,8 @@
 var Bank = require("../bank");
 describe('Bank', function() {
   var bank;
-  beforeEach(function() {
+  const make_deposit = (bank, amount) => bank.deposit(amount);
+;  beforeEach(function() {
      bank = new Bank();
   });
   it("has a balance of 0 on initialization", function() {
@@ -12,14 +13,21 @@ describe('Bank', function() {
   });
   it('it increases the bank balance by a given amount as long as it is a numerical value', function() {
     // arrange
-    bank.deposit(10);
+    make_deposit(bank, 10);
     // assert
     expect(bank.balance).toEqual(10);
 
   });
   it('has a function that will print the curent balance of the bank', function() {
     // arrange
-    bank.deposit(10);
+    make_deposit(bank, 10);
+    // assert
+    expect(bank.print_balance()).toEqual('Current balance: £10');
+
+  });
+  it('prints a confirmation of a successful deposit', function() {
+    // arrange
+    make_deposit(bank, 5);
     // assert
     expect(bank.print_balance()).toEqual('Current balance: £10');
 
